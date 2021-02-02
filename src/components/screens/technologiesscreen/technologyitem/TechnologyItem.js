5b449ff9568a5f5e09869990b3f74a3393bcb02e
Juan Jose Mayorga <tmaniacus@gmail.com>
2021-02-01 19:32:46 -0600

    technologies screen for mobile finished

--- src/components/screens/technologiesscreen/technologyitem/TechnologyItem.js
+++ src/components/screens/technologiesscreen/technologyitem/TechnologyItem.js
@@ -1,20 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './technologyitem.css'
 
 export const TechnologyItem = ({ technology }) => {
-
-  const [techItemClicked, setTechItemClicked] = useState(false)
-
-  const handleTechItemClick = _ => setTechItemClicked(!techItemClicked)
 
   const { image, name } = technology
 
   return (
     <div
-      onClick={handleTechItemClick}
       className="technology-screen-item-container">
       <figure
-        className={techItemClicked ? "technology-screen-item__figure--clicked" : "technology-screen-item__figure"}>
+        className="technology-screen-item__figure">
         <img
           className="technology-screen-item__image"
           src={image}
@@ -22,7 +17,7 @@ export const TechnologyItem = ({ technology }) => {
           title={name}/>
       </figure>
       <p
-        className={techItemClicked ? "technology-screen-item__technology-text--clicked" : "technology-screen-item__technology-text"}>{name}</p>
+        className="technology-screen-item__technology-text">{name}</p>
     </div>
   )
 }
